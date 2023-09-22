4e955918fa5874c3ed13deb6952832d04bef5be0
LaylaSumeru2004 <70891263+LaylaSumeru2004@users.noreply.github.com>
2023-09-22 10:59:02 +0700

    Set up handler to execute the MongoDB database

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require("./app");
 const config = require("./app/config");
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT + '.');
-});
+const MongoDB = require("./app/utils/mongodb.util");
+async function startServer(){
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Database was connected successfully!");
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log('Server is running on port ' + PORT + '.');
+        });
+    } catch (error){
+        console.log("Failed connecting to database!", error);
+        process.exit();
+    }
+}
+startServer();
